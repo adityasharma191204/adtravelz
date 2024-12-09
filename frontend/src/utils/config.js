566cc0b9bd3aae93f1fca8fcdee044f919ea307d
adityasharma191204 <117986637+adityasharma191204@users.noreply.github.com>
2024-12-09 18:22:57 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://trips-travel-server.vercel.app/api";
-const BASE_URL = "https://adtravelz-yh74.onrender.com";
+const BASE_URL = "https://adtravelz-frontend.onrender.com/api";
+// const BASE_URL = "http://localhost:3050/api";
 //
 export default BASE_URL;
